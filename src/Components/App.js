6c97c6dae72afa2aaea784b87c7e9042a609dc2e
Phil Lewis <phillewis1985@gmail.com>
2022-05-16 09:33:43 -0600

    Install router and start About component

---
+++ src/Components/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../Styles/App.css'
+import ''
+
+const App = () => {
+  return (
+    <main>
+      <header>
+        <h1>Phil Lewis</h1>
+        <p>Front End Software Engineer</p>
+      </header>
+
+    </main>
+  )
+}
+
+export default App;
