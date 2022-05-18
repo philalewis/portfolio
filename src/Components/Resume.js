487bd69ef34c328c77370ba1a2544d9628675366
Phil Lewis <phillewis1985@gmail.com>
2022-05-18 11:25:26 -0600

    Create button and enable download resume when clicked

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -1,22 +1,20 @@
 import React from 'react'
 import '../Styles/Resume.css'
-import { Document } from 'react-pdf'
-// import '../PhilLewisResume.png'
-import '../PhilLewisResume.pdf'
-
-// import { Document, Page, pdfjs } from "react-pdf";
-
-// pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
 
 const Resume = () => {
   return (
     <section className="resume-page">
-      <Document file={'../PhilLewisResume.pdf'} />
-      {/* <img
+      <img
         src={require('../PhilLewisResume.png')}
         alt="resume"
         className="resume-image"
-      /> */}
+      />
+        <a
+          href='PhilLewisResume.pdf'
+          download
+        >
+          <button>Download</button>
+        </a>
     </section>
   )
 }
