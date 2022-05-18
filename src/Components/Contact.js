55fa09ed95817e0095937344dd21ab4103847c67
Phil Lewis <phillewis1985@gmail.com>
2022-05-17 18:09:05 -0600

    Add subject field and onClick method to submit button

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -5,9 +5,10 @@ const Contact = () => {
   const [ name, setName ] = useState('')
   const [ email, setEmail ] = useState('')
   const [ message, setMessage ] = useState('')
+  const [ subject, setSubject ] = useState('')
 
   const sendEmail = () => {
-    
+    window.open(`mailto:phillewis1985@gmail.com?subject=${subject}&body=${message}`)
   }
 
   return (
@@ -17,22 +18,32 @@ const Contact = () => {
           type='text'
           value={name}
           className="input-field"
+          placeholder="name"
           onChange={event => setName(event.target.value)}
         />
         <input
           type='email'
           value={email}
           className="input-field"
+          placeholder="email address"
           onChange={event => setEmail(event.target.value)}
+        />
+        <input
+          type='text'
+          value={subject}
+          className="input-field"
+          placeholder="subject"
+          onChange={event => setSubject(event.target.value)}
         />
         <input
           type='text'
           value={message}
           className="input-field"
+          placeholder="message"
           onChange={event => setMessage(event.target.value)}
         />
         <button
-        className="send-email-button"
+          className="send-email-button"
           onClick={event => sendEmail(event)}
         >Send</button>
       </form>
