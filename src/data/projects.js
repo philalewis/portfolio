d2c0d136737465ce65f65906c3e5cc76164a0f44
Phil Lewis <phillewis1985@gmail.com>
2022-05-18 11:42:21 -0600

    Create files and data

---
+++ src/data/projects.js
@@ -0,0 +1,60 @@
+export const projects = [
+  {
+    name: 'One Minute Writer',
+    description: '',
+    technologies: [
+      'React',
+      'TypeScript',
+      'GraphQL',
+      'REST',
+      'SCSS',
+      'Cypress'
+    ],
+    deployed: '',
+    repo: '',
+    backend: ''
+  },
+  {
+    name: 'Jazz Collaborations',
+    description: '',
+    technologies: [
+      'React',
+      'JavaScript',
+      'REST',
+      'Express',
+      'SCSS',
+      'Cypress'
+    ],
+    deployed: '',
+    repo: '',
+    backend: ''
+  },
+  {
+    name: 'Discify',
+    description: '',
+    technologies: [
+      'React',
+      'JavaScript',
+      'REST',
+      'SCSS',
+      'Cypress'
+    ],
+    deployed: '',
+    repo: '',
+    backend: ''
+  },
+  {
+    name: 'Rancid Tomatillos',
+    description: '',
+    technologies: [
+      'React',
+      'JavaScript',
+      'REST',
+      'SCSS',
+      'Cypress'
+    ],
+    deployed: '',
+    repo: '',
+    backend: ''
+  }
+]
