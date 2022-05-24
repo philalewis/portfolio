d8cd889d08e9c9d0cee416a85c1f8e33a1a5e96c
Phil Lewis <phillewis1985@gmail.com>
2022-05-23 20:30:54 -0600

    Import contact logos and style

--- src/Components/App.js
+++ src/Components/App.js
@@ -6,6 +6,9 @@ import Navbar from './Navbar'
 import Contact from './Contact'
 import Resume from './Resume'
 import Projects from './Projects'
+import emailLogo from '../images/email-logo.svg'
+import githubLogo from '../images/Github-logo.png'
+import linkedinLogo from '../images/linkedin-logo.svg'
 
 const App = () => {
   return (
@@ -13,6 +16,29 @@ const App = () => {
       <header>
         <h1>Phil Lewis</h1>
         <p>Front End Software Engineer</p>
+        <section className="contact-info-section">
+          <button className="contact-logo-button">
+            <img
+              className="contact-logo"
+              src={emailLogo}
+              alt="email logo"
+            />
+          </button>
+          <button className="contact-logo-button">
+            <img
+              className="contact-logo"
+              src={githubLogo}
+              alt="github logo"
+            />
+          </button>
+          <button className="contact-logo-button">
+            <img
+              className="contact-logo"
+              src={linkedinLogo}
+              alt="linkedin logo"
+            />
+          </button>
+        </section>
         <Navbar />
       </header>
       <Routes>
--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,27 +1,30 @@
 import React, { useState } from 'react'
 import '../Styles/Contact.css'
+import emailLogo from '../images/email-logo.svg'
+import githubLogo from '../images/Github-logo.png'
+import linkedinLogo from '../images/linkedin-logo.svg'
 
 const Contact = () => {
-  const [ name, setName ] = useState('')
-  const [ email, setEmail ] = useState('')
-  const [ message, setMessage ] = useState('')
-  const [ subject, setSubject ] = useState('')
+  // const [ name, setName ] = useState('')
+  // const [ email, setEmail ] = useState('')
+  // const [ message, setMessage ] = useState('')
+  // const [ subject, setSubject ] = useState('')
 
-  const sendEmail = () => {
-    window.open(`mailto:phillewis1985@gmail.com?subject=${subject}&body=${message}`)
-  }
+  // const sendEmail = () => {
+  //   window.open(`mailto:phillewis1985@gmail.com?subject=${subject}&body=${message}`)
+  // }
 
   return (
     <section className="contact-page">
       <form className="contact-form">
-        <input
+        {/* <input
           type='text'
           value={name}
           className="input-field"
           placeholder="name"
           onChange={event => setName(event.target.value)}
-        />
-        <input
+        /> */}
+        {/* <input
           type='email'
           value={email}
           className="input-field"
@@ -41,11 +44,11 @@ const Contact = () => {
           className="input-field"
           placeholder="message"
           onChange={event => setMessage(event.target.value)}
-        />
+        /> */}
         <button
           className="send-email-button"
-          onClick={event => sendEmail(event)}
-        >Send</button>
+          // onClick={event => sendEmail(event)}
+        >Email</button>
       </form>
     </section>
   )
